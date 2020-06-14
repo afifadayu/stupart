5f7025bc550dd7c056dd847cd002ef556eb0913e
Afifa Ayu Widhiyanthi <afifaaayu@gmail.com>
2020-06-14 13:13:12 +0700

    front end upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ChatScreen from './screens/ChatScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={ HomeScreen }
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Chat"
+          component={ ChatScreen }
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ ProfileScreen }
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
